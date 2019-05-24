59f2de7c92d42be8a98f4dadf8cc4747b4cdc96b
T99 <trevorsears.main@gmail.com>
2019-05-24 11:48:41 -0400

    Temp fix for kiosk progression.

--- js/ts/af-activities/af-kiosk-activity.ts
+++ js/ts/af-activities/af-kiosk-activity.ts
@@ -5,8 +5,13 @@
  */
 
 import AUIKioskPage from "../aui/pages/kiosk/aui-kiosk-page.js";
-import TSArrayList from "../structures/implementations/list/ts-array-list.js";
+import TSArrayList from "../util/structures/implementations/list/ts-array-list.js";
 import AFFolder from "../af-structures/structures/af-folder.js";
+import AFQuestion from "../af-structures/structures/af-question.js";
+import AUIQuestion from "../aui/pages/kiosk/questions/aui-question.js";
+import JUIWorld from "../jui/jui-world.js";
+import AUIKioskThankYouPage from "../aui/pages/thank-you/aui-kiosk-thank-you-page.js";
+import AUIContactCapturePage from "../aui/pages/contact-capture/aui-contact-capture-page.js";
 
 /**
  *
@@ -23,15 +28,19 @@ class AFKioskActivity {
 	
 	private upcomingFolder: AFFolder;
 	
-	private questionList: TSArrayList;
+	private questionList: TSArrayList<AFQuestion>;
+	
+	private currentQuestionIndex: number;
 	
 	private constructor(folder: AFFolder) {
 		
 		this.currentFolder = folder;
+		this.questionList = new TSArrayList<AFQuestion>();
+		this.currentQuestionIndex = 0;
 		
 	}
 	
-	public async create(folder: AFFolder): Promise<AFKioskActivity> {
+	public static async create(folder: AFFolder): Promise<AFKioskActivity> {
 		
 		if (folder.getQuestionListing().size() === 0) {
 			
@@ -41,13 +50,74 @@ class AFKioskActivity {
 			
 			let kioskActivity: AFKioskActivity = new AFKioskActivity(folder);
 			
-			await kioskActivity.reinitializeQuestionList();
-			
-			kioskActivity.kioskPage = new AUIKioskPage(this.questionList.get(0));
+			await kioskActivity.renew();
 			
 			return kioskActivity;
 			
 		}
+		
+	}
+	
+	private static async setupKioskPage(activity: AFKioskActivity): Promise<AUIKioskPage> {
+		
+		let kioskPage: AUIKioskPage = new AUIKioskPage(
+			AUIQuestion.createForQuestion(activity.questionList.get(0)),
+			activity.questionList.size()
+		);
+		
+		kioskPage.subscribeToBackButton((): void => console.log("Back button clicked."));
+		kioskPage.subscribeToSkipButton((): void => console.log("Skip button clicked."));
+		kioskPage.subscribeToDoneButton((): void => console.log("Done button clicked."));
+		
+		kioskPage.subscribeToDoneButton((): void => {
+			
+			if (activity.questionList.size() - 1 > activity.currentQuestionIndex) {
+				
+				kioskPage.nextQuestion(
+					AUIQuestion.createForQuestion(activity.questionList.get(++activity.currentQuestionIndex)),
+					activity.currentQuestionIndex + 1
+				);
+				
+			} else {
+				
+				// JUIWorld.getInstance().goToPageRight(new AUIContactCapturePage());
+				
+				AUIKioskThankYouPage.sayThankYou().then(() => {
+
+					activity.renew();
+
+				});
+				
+			}
+			
+		});
+		
+		kioskPage.subscribeToBackButton((): void => {
+			
+			if (activity.currentQuestionIndex > 0) {
+				
+				kioskPage.previousQuestion(
+					AUIQuestion.createForQuestion(activity.questionList.get(--activity.currentQuestionIndex)),
+					activity.currentQuestionIndex + 1
+				);
+				
+			}
+			
+		});
+		
+		return kioskPage;
+		
+	}
+	
+	private async renew(): Promise<void> {
+		
+		await this.reinitializeQuestionList();
+		
+		this.kioskPage = await AFKioskActivity.setupKioskPage(this);
+		
+		await JUIWorld.getInstance().goToPageRight(this.kioskPage);
+		
+		this.kioskPage.resizeInquiryText();
 		
 	}
 	
--- js/ts/aui/pages/thank-you/aui-kiosk-thank-you-page.ts
+++ js/ts/aui/pages/thank-you/aui-kiosk-thank-you-page.ts
@@ -6,12 +6,11 @@
 
 import JUIPage from "../../../jui/jui-page.js";
 import JUIAlignment from "../../../jui/descriptors/jui-alignment.js";
-import JUIStackContainer from "../../../jui/elements/multi-containers/jui-stack-container.js";
+import JUIStackContainer from "../../../jui/elements/containers/multi-containers/jui-stack-container.js";
 import JUIImageLeaf from "../../../jui/elements/leaves/content-leaves/jui-image-leaf.js";
 import AUIKioskTextLabel from "../kiosk-old/aui-kiosk-text-label.js";
+import TSAwaitUtils from "../../../util/structures/implementations/ts-await-utils.js";
 import JUIWorld from "../../../jui/jui-world.js";
-import AFFolder from "../../../af-structures/structures/af-folder.js";
-import AUIKioskPageOld from "../kiosk-old/aui-kiosk-page-old.js";
 
 /**
  *
@@ -29,11 +28,11 @@ class AUIKioskThankYouPage extends JUIPage {
 	 */
 	public readonly TYPE_IDENTITY: string = "aui-kiosk-thank-you-page";
 	
-	private static readonly TIMEOUT: number = 5000;
+	private static readonly DEFAULT_TIMEOUT: number = 5000;
 	
 	private stackContainer: JUIStackContainer;
 	
-	public constructor(nextFolder: AFFolder) {
+	private constructor() {
 	
 		super();
 		this.addClasses(this.TYPE_IDENTITY);
@@ -44,13 +43,17 @@ class AUIKioskThankYouPage extends JUIPage {
 		this.stackContainer.addStackedChild(new AUIKioskTextLabel("Thanks for helping!"));
 		
 		this.setChild(this.stackContainer);
-		
-		setTimeout(() => {
-
-			JUIWorld.getInstance().goToPageRight(new AUIKioskPageOld(nextFolder));
-
-		}, AUIKioskThankYouPage.TIMEOUT);
 	
+	}
+	
+	public static async sayThankYou(wait: number = AUIKioskThankYouPage.DEFAULT_TIMEOUT): Promise<void> {
+		
+		let thankYouPage: AUIKioskThankYouPage = new AUIKioskThankYouPage();
+		
+		await JUIWorld.getInstance().goToPageRight(thankYouPage);
+		
+		await TSAwaitUtils.ms(wait);
+		
 	}
 	
 }
